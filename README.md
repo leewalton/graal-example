f04576f8ddba97a8ef89fef0b85fddb9a6d4d87e
Lee Walton <leewalton@gmail.com>
2020-06-14 09:25:43 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Graal Example
+
+This is a very simple example of a Hello World application which should be build using the Graal 20.1 JDK 11.
+
+To build: `mvn clean pack`
---
+++ src/main/java/net/deeptweetbot/example/Application.java
@@ -0,0 +1,9 @@
+package net.deeptweetbot.example;
+
+class Application {
+
+    public static void main(String... args) {
+        System.out.println("Hello from Graal!");
+    }
+
+}
