7b34962079714bc39dcad95456318411f1f8e703
Lee Walton <leewalton@gmail.com>
2020-06-14 11:18:28 +0100

    Added logback as an example of resource & reflection configuration

--- src/main/java/net/deeptweetbot/example/Application.java
+++ src/main/java/net/deeptweetbot/example/Application.java
@@ -1,9 +1,15 @@
 package net.deeptweetbot.example;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 class Application {
 
+    private static final Logger logger
+            = LoggerFactory.getLogger(Application.class);
+
     public static void main(String... args) {
-        System.out.println("Hello from Graal!");
+        logger.info("Hello from Graal!");
     }
 
 }
